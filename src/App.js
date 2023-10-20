8a66993ef49ad28e23f4665bbf533ba4fb5b2db0
Reiner Terry <dragoreiner@gmail.com>
2023-10-20 22:57:53 +0700

    initial setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{padding : 0, margin:0}}>HELLO WORLD</h1>
+    </div>
+  );
+}
+
+export default App;
